3595aabc18605108f62946804d2bbf4363773c8d
A Glazer <avigayilglazer@gmail.com>
2019-02-13 19:28:45 +0000

    Done.

--- lib/cat.rb
+++ lib/cat.rb
@@ -1,3 +1,13 @@
 class Cat
-  # code goes here
+  attr_accessor :mood
+  attr_reader :name #reading and writing instance properties
+#reader - set it upon initialization and never change it
+
+#initializing a new instance => .new
+  def initialize(name) #called when .new is used. only need an intialize method if we want to set properties upon iniitialization
+    @name = name
+    @mood = "nervous"
+  end
+
+
 end
