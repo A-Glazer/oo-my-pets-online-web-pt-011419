caf11127e73bac2ae70f1cfb02563bfb65d3f39b
A Glazer <avigayilglazer@gmail.com>
2019-02-13 20:21:26 +0000

    Done.

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,3 +1,37 @@
 class Owner
-  # code goes here
-end
+  attr_accessor :name
+  attr_reader :species
+   @@all = []
+
+  def initialize (name)
+    @owner = []
+    @owner << name
+    @@all << self
+
+
+  end
+
+   def self.all
+     @@all
+   end
+
+   def self.count
+     @@all.size
+   end
+
+   def self.reset_all
+     @@all = []
+   end
+
+   def species
+     @species = "human"
+   end
+
+   def say_species
+     "I am a #{species}."
+   end
+
+   def name
+     @name = "Katie"
+   end
+  end
